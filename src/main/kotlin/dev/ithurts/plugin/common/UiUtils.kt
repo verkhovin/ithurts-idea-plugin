9ae5bd09348ecd958e6d86dd7293391090cb2632
Nikita Verkhovin <verkhovin13@gmail.com>
2022-08-10 00:46:06 +0300

    fix report debt tool window

--- src/main/kotlin/dev/ithurts/plugin/common/UiUtils.kt
+++ src/main/kotlin/dev/ithurts/plugin/common/UiUtils.kt
@@ -2,14 +2,14 @@ package dev.ithurts.plugin.common
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindowManager
-import dev.ithurts.plugin.ide.toolwindow.ReportDebtToolWindow
+import dev.ithurts.plugin.ide.toolwindow.ReportDebtToolWindowOld
 
 object UiUtils {
     fun rerenderReportDebtToolWindow(project: Project) {
         val toolWindow = getReportDebtToolWindow(project)
         val contentManager = toolWindow.contentManager
         contentManager.removeAllContents(true)
-        val reportDebtToolWindow = ReportDebtToolWindow(project)
+        val reportDebtToolWindow = ReportDebtToolWindowOld(project)
         contentManager.addContent(reportDebtToolWindow.getContent())
         toolWindow.activate(null)
     }
--- src/main/kotlin/dev/ithurts/plugin/ide/toolwindow/ReportDebtToolWindow.kt
+++ src/main/kotlin/dev/ithurts/plugin/ide/toolwindow/ReportDebtToolWindow.kt
@@ -5,95 +5,147 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.ui.ComboBox
+import com.intellij.ui.JBColor
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.components.JBTextArea
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
-import com.intellij.ui.layout.CellBuilder
-import com.intellij.ui.layout.panel
 import dev.ithurts.plugin.common.FileUtils
 import dev.ithurts.plugin.common.UiUtils
+import dev.ithurts.plugin.common.swing.MouseListenerWrapper
+import dev.ithurts.plugin.common.swing.SimpleDocumentListener
+import dev.ithurts.plugin.ide.model.Binding
+import dev.ithurts.plugin.ide.model.EditableDebt
 import dev.ithurts.plugin.ide.service.debt.DebtReportingService
+import dev.ithurts.plugin.ide.service.debt.ItHurtsGitRepositoryService
 import dev.ithurts.plugin.ide.service.debt.StageMode
 import dev.ithurts.plugin.ide.service.debt.StagedDebtService
-import java.awt.event.FocusEvent
-import java.awt.event.FocusListener
+import net.miginfocom.swing.MigLayout
+import java.awt.Cursor
+import java.awt.event.MouseEvent
+import javax.swing.JButton
 import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.event.DocumentEvent
+import kotlin.reflect.KMutableProperty0
 
 
-class ReportDebtToolWindow(private val project: Project) {
+class ReportDebtToolWindowOld(private val project: Project) {
     private val stagedDebtService = project.service<StagedDebtService>()
     private val debtReportingService = project.service<DebtReportingService>()
+    private val mainBranch: String = project.service<ItHurtsGitRepositoryService>().mainBranch
 
-    private var rootPanel: DialogPanel? = null
-
+    private val emptyRoot = JPanel(MigLayout("fill"))
+    private val root = JPanel(MigLayout("fillx", "[]", "[][fill,grow][][]"))
+    private val cancelEditingButton = link("Cancel editing") {
+        stagedDebtService.cancelEditing()
+        UiUtils.hideReportDebtToolWindow(project)
+    }
+    private val titleField = JBTextField()
+    private val descriptionField = JBTextArea()
+    private val bindingField = ComboBox<Binding>()
+    private val reportButton = JButton("Submit")
 
     fun getContent(): Content {
         val stagedDebt = stagedDebtService.stagedDebt
-        rootPanel = if (stagedDebt == null || stagedDebt.bindings.isEmpty()) {
-            panel {
-                row {
-                    label("Nothing is selected. Select some code -> right click -> \"Add Debt Binding\"")
-                }
-            }
-        } else {
-            panel {
-                if (stagedDebtService.stageMode == StageMode.EDIT) {
-                    row {
-                        link("Cancel editing") {
-                            stagedDebtService.cancelEditing()
-                            UiUtils.hideReportDebtToolWindow(project)
-                        }
-                    }
-                }
-                row {
-                    textField(stagedDebt::title)
-                        .applyPanelOnLostFocus { rootPanel!! }
-                }
-                row {
-                    textArea(stagedDebt::description, 40, 10).applyPanelOnLostFocus { rootPanel!! }
-                }
-                titledRow("Bindings:") {
-                    stagedDebt.bindings.map { binding ->
-                        row {
-                            link(binding.toString()) {
-                                val file = FileUtils.virtualFileByPath(project, binding.filePath)
-                                ApplicationManager.getApplication().invokeLater {
-                                    FileEditorManager.getInstance(project).openTextEditor(
-                                        OpenFileDescriptor(project, file, binding.lines.first - 1, 0), true
-                                    )
-                                }
-                            }
-                            link("X") {
-                                stagedDebtService.removeBinding(binding)
-                                UiUtils.rerenderReportDebtToolWindow(project)
-                            }
-                        }
-                    }
-                }
-                row {
-                    button("Submit") {
-                        rootPanel!!.apply()
-                        debtReportingService.reportDebt()
-                    }
-                }
-
-            }
+        if (stagedDebt == null) {
+            emptyRoot.add(
+                JLabel("Nothing is selected. Select some code -> right click -> \"Add Debt Binding\""),
+                "align center"
+            )
+            return wrapToContent(emptyRoot)
         }
-        return ContentFactory.SERVICE.getInstance().createContent(rootPanel!!, "", false)
+
+        setValues(stagedDebt)
+
+        reportButton.addActionListener { debtReportingService.reportDebt() }
+        titleField.document.addDocumentListener(ValueBindingDocumentListener(stagedDebt::title) {titleField.text})
+        descriptionField.document.addDocumentListener(ValueBindingDocumentListener(stagedDebt::description) {descriptionField.text})
+
+        if (stagedDebtService.stageMode == StageMode.EDIT) {
+            root.add(cancelEditingButton, "grow, span")
+        }
+
+        val descrPanel = JPanel(MigLayout("fillx", "[fill]", "[fill]"))
+        descrPanel.add(descriptionField)
+        root.add(titleField, "grow, span")
+        root.add(descriptionField, "growx, wmin 10, span")
+        bindings(stagedDebt, root)
+        submitSection(stagedDebt, root)
+        return wrapToContent(root)
     }
 
-    private fun <T : JComponent> CellBuilder<T>.applyPanelOnLostFocus(panel: () -> DialogPanel): CellBuilder<T>  {
-        this.component.addFocusListener(ApplyPanelOnFocusListener(panel))
-        return this
+    private fun bindings(stagedDebt: EditableDebt, panel: JPanel) {
+        stagedDebt.bindings.forEach { binding ->
+            panel.add(link(binding.toString()) {
+                val file = FileUtils.virtualFileByPath(project, binding.filePath)
+                ApplicationManager.getApplication().invokeLater {
+                    FileEditorManager.getInstance(project).openTextEditor(
+                        OpenFileDescriptor(project, file, binding.lines.first - 1, 0), true
+                    )
+                }
+            }, "split 2")
+            panel.add(link("X") {
+                stagedDebtService.removeBinding(binding)
+                UiUtils.rerenderReportDebtToolWindow(project)
+            }, "wrap")
+        }
+    }
+
+    private fun submitSection(stagedDebt: EditableDebt, panel: JPanel) {
+        if (stagedDebt.bindings.any { it.advancedBinding == null }) {
+            panel.add(JBCheckBox("Fetch $mainBranch branch before submit"), "align right, split 2")
+            panel.add(link("?", false) {}.also { it.toolTipText = "Debt bindings reference source code stored in the main repository branch. " +
+                    "Fetching the main branch before submitting the debt lets It Hurts Integration plugin adjust " +
+                    "the submission relative to the actual code." }, "align right, wrap")
+        }
+        panel.add(reportButton,"align right, wrap")
+    }
+
+    private fun setValues(stagedDebt: EditableDebt) {
+        bindingField.removeAllItems()
+        stagedDebt.bindings.forEach { binding -> bindingField.addItem(binding) }
+
+
+        titleField.emptyText.text = "Title"
+        descriptionField.emptyText.text = "Description"
+        descriptionField.lineWrap = true
+
+        if (stagedDebt.title.isNotBlank()) {
+            titleField.text = stagedDebt.title
+        }
+        if (stagedDebt.description.isNotBlank()) {
+            descriptionField.text = stagedDebt.description
+        }
+    }
+
+    private fun wrapToContent(component: JComponent): Content {
+        val contentFactory = ContentFactory.SERVICE.getInstance()
+        return contentFactory.createContent(component, "", false)
+    }
+
+    companion object {
+        fun link(text: String, handCursor: Boolean = true, onClick: () -> Unit): JLabel {
+            val label = JLabel(text)
+            label.foreground = JBColor.BLUE
+            if (handCursor) {
+                label.cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
+            }
+            label.addMouseListener(object: MouseListenerWrapper {
+                override fun mouseClicked(e: MouseEvent?) {
+                    onClick()
+                }
+            })
+            return label
+        }
     }
 }
 
-class ApplyPanelOnFocusListener(private val panel: () -> DialogPanel): FocusListener {
-    override fun focusGained(e: FocusEvent?) {
-
-    }
-
-    override fun focusLost(e: FocusEvent?) {
-        panel().apply()
+class ValueBindingDocumentListener(private val property: KMutableProperty0<String>, private val source: () -> String): SimpleDocumentListener {
+    override fun onChange(e: DocumentEvent) {
+        property.set(source())
     }
 }
